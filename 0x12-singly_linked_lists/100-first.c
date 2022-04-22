eefe072345e98d01e29e835c6fccccbc3b3d97e3
Adaora Okwo <adaorao60@gmail.com>
2022-04-21 17:23:23 -0800

    Correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * printf - function executed before main
+ * bmain - function executed before main
 */
 
-void printf(void) __attribute__ ((constructor));
+void bmain(void) __attribute__ ((constructor));
 
-void printf(void)
+void bmain(void)
 {
-	printf("You're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
 int main(void)
